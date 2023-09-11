0310cd68f5a5e548da8b9bc9904ae6bdbe31d4e0
Damian Dziaduch <damian@dziaduch.pl>
2023-09-11 16:05:11 +0200

    Add routing

---
+++ config/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use ddziaduch\PenaltyPoints\Adapters\Primary\ImposePenaltyHttpAdapter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return static function (RoutingConfigurator $configurator): void {
+    $configurator->add(
+        ImposePenaltyHttpAdapter::class,
+        '/impose-penalty/driver/{driverLicenseNumber}/points/{numberOfPoints}',
+    )->requirements([
+        'driverLicenseNumber' => '[a-zA-Z0-9]+',
+        'numberOfPoints' => '/d+'
+    ])->methods([Request::METHOD_POST])->controller(ImposePenaltyHttpAdapter::class);
+};
